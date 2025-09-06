82e4d1eaaad25afde64e2b740d4f39adc77cfc7d
Soumya Deb <debloper@gmail.com>
2025-09-06 09:47:03 +0530

    🔨⚡✨(build): update package versions, add SRT support

--- setup.sh
+++ setup.sh
@@ -4,20 +4,22 @@
 export WORKDIR=$(pwd)
 
 # create a temporary working directory
-mkdir build
+mkdir -p build
 cd build
 
 # download the source code for nginx, openssl, pcre, rtmp, and zlib
-export v_NGINX=1.26.1
-export vm_OSSL=1.1.1w
-export vm_PCRE=8.45
+export v_NGINX=1.29.1
+export vm_OSSL=3.5.0
+export vm_PCRE=10.46
 export vm_RTMP=1.2.2
+export vm_SRTP=1.1
 export vm_ZLIB=1.3.1
 
 wget https://nginx.org/download/nginx-${v_NGINX}.tar.gz
-wget https://www.openssl.org/source/openssl-${vm_OSSL}.tar.gz
-wget https://sourceforge.net/projects/pcre/files/pcre/${vm_PCRE}/pcre-${vm_PCRE}.tar.gz
+wget https://github.com/openssl/openssl/releases/download/openssl-${vm_OSSL}/openssl-${vm_OSSL}.tar.gz
+wget https://github.com/PCRE2Project/pcre2/releases/download/pcre2-${vm_PCRE}/pcre2-${vm_PCRE}.tar.gz
 wget https://github.com/arut/nginx-rtmp-module/archive/v${vm_RTMP}.tar.gz
+wget https://github.com/kaltura/nginx-srt-module/archive/refs/tags/v${vm_SRTP}.tar.gz
 wget https://zlib.net/fossils/zlib-${vm_ZLIB}.tar.gz
 
 # extract the source code
@@ -28,13 +30,16 @@ cd ${WORKDIR}/build/nginx-${v_NGINX}
 
 ./configure \
     --with-openssl=../openssl-${vm_OSSL} \
-    --with-pcre=../pcre-${vm_PCRE} \
+    --with-pcre=../pcre2-${vm_PCRE} \
     --with-zlib=../zlib-${vm_ZLIB} \
-    --add-module=../nginx-rtmp-module-${vm_RTMP}
+    --add-module=../nginx-rtmp-module-${vm_RTMP} \
+    --add-module=../nginx-srt-module-${vm_SRTP} \
+    --with-stream --with-threads
 
 # compile and install nginx
-make
+make -j"$(nproc)"
 make install
+strip /usr/local/nginx/sbin/nginx
 
 # update nginx configuration to use xplex
 cp ${WORKDIR}/conf/*.conf /usr/local/nginx/conf/
